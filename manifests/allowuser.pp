f0ca1ff1f5c6eaaa83319ecebdeb339086cd3fba
Jordi Prats <jprats@systemadmin.es>
2017-07-11 12:14:58 +0200

    incude atd

--- manifests/allowuser.pp
+++ manifests/allowuser.pp
@@ -1,6 +1,8 @@
 # puppet2sitepp @atdallowusers
 define atd::allowuser($username = $name) {
 
+  include ::atd
+
   concat::fragment{ "/etc/at.allow ${username}":
     target  => '/etc/at.allow',
     order   => '42',
