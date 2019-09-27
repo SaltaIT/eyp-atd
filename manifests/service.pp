363f445d574fa75aa326257058627df42a9639d0
Jordi Prats <jordi.prats@gmail.com>
2019-09-27 12:39:31 +0200

    compatibility

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,5 @@
 class atd::service inherits atd {
 
-  #
-  validate_bool($atd::manage_docker_service)
-  validate_bool($atd::manage_service)
-  validate_bool($atd::service_enable)
-
-  validate_re($atd::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${atd::service_ensure}")
-
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
 
